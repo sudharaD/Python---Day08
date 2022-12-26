b65b4374fa63cfc01b772beeea30899a890280d8
sudharaD <sudharad7@gmail.com>
2022-12-26 18:33:20 +0530

    Decode part start

--- caesar_cipher.py
+++ caesar_cipher.py
@@ -1,28 +1,73 @@
-import caesar_cipher_ascii_art
+from caesar_cipher_resources import alphabet
+from caesar_cipher_resources import characters
+from caesar_cipher_resources import logo
 
 encripted_letters_list = []
-alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+run_again = True
 
+print(logo)
 
-print(caesar_cipher_ascii_art.ascii_art)
+while run_again:
 
-function = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
-message = input("Type your message\n").lower()
-shift_number = int(input("Type the shift number:\n"))
+    function = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+    message = input("Type your message\n").lower()
+    shift_number = int(input("Type the shift number:\n"))
 
-# Encode function
-def encode(message, shift_number):
-    for i in range(len(message)):
+    # Encode letters
+    def letter_encode(i):
         for j in range(len(alphabet)):
             if alphabet[j] == message[i]:
                 if (j + shift_number) > 25:
                     encripted_letters_list.append(alphabet[j+shift_number-26])
                 else:
                     encripted_letters_list.append(alphabet[j+shift_number])
-    
-    print(encripted_letters_list)
 
-if function.lower() == "encode":
-    encode(message = message, shift_number = shift_number)
-# else:
-#     decode()
+    # Encode characters
+    def character_encode(i):
+        for k in range(len(characters)):
+            if characters[k] == message[i]:
+                if (k + shift_number) > 31:
+                    encripted_letters_list.append(characters[k+shift_number-32])
+                else:
+                    encripted_letters_list.append(characters[k+shift_number])
+
+    # Encode function
+    def encode(message, shift_number):
+        for i in range(len(message)):
+            if message[i] in alphabet:
+                letter_encode(i)
+            elif message[i] == " ":
+                encripted_letters_list.append(" ")
+            else:
+                character_encode(i)
+        
+        encripted_letters_list_to_string = "".join(encripted_letters_list)
+        print(f"Here's the encoded result: {encripted_letters_list_to_string}")
+
+    # Encode function
+    def decode(message, shift_number):
+        for i in range(len(message)):
+            if message[i] in alphabet:
+                letter_encode(i)
+            elif message[i] == " ":
+                encripted_letters_list.append(" ")
+            else:
+                character_encode(i)
+        
+        encripted_letters_list_to_string = "".join(encripted_letters_list)
+        print(f"Here's the encoded result: {encripted_letters_list_to_string}")
+
+    if function.lower() == "encode":
+        encode(message = message, shift_number = shift_number)
+    elif function.lower() == "decode":
+        decode(message = message, shift_number = shift_number)
+    else:
+        print("Please type correct answer")
+
+    proceed = input("Type 'yes' if you want to go again., Otherwise typr 'no':\n").lower()
+
+    # Check user want to continue or exit
+    if proceed == "yes":
+        run_again = True
+    else:
+        run_again = False
